7bebd77d38534899f5ab290ff7fa9a323460f5eb
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2024-04-10 16:06:07 +0900

    tasksのモジュール作成,graphqlのschema作成

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // クライアントから送られてきたrequestのbodyを検証（バリデーションを有効化）するためのパイプを追加
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  app.enableCors();
 }
 bootstrap();
