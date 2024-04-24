db1de6a2bbd09c4d988b077b28fd2d432f4bea2a
Naoyuki Kojima <116235104+ronaudo9@users.noreply.github.com>
2024-04-24 15:08:54 +0900

    launch.jsonファイルを追加 (#5)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: '6h' },
     }),
   ],
   providers: [AuthResolver, AuthService, LocalStrategy, JwtStrategy],
